31d6033a82bcc79e979251c9f897aa456ed86d96
Raxzero <emircan.acikgoz@hotmail.com>
2024-08-22 18:28:29 +0300

    baby steps

--- MainWindow.xaml.vb
+++ MainWindow.xaml.vb
@@ -1,3 +1,14 @@
-﻿Class MainWindow
+﻿Imports System.Data.SqlClient
+Imports System.Data
 
+Class MainWindow
+    Private Sub MainWindow_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+        Dim connectionString As String = "Data Source=\DESENERP;Initial Catalog=DesenPOS; Persist Security Info=False; TrustServerCertificate=True; uid=sa; password=DesenErp.12345"
+        Dim query As String = "SELECT * FROM POSSiparis"
+        Using connection As New SqlConnection(connectionString
+                                              )
+            Dim command As New SqlCommand(query)
+
+        End Using
+    End Sub
 End Class
